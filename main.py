00fb70e1e8bf1be1c0388a0aa8beae832704f89e
Elliot Arledge <59771030+InfectiousFN@users.noreply.github.com>
2021-01-22 09:47:53 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# Input
+
+num1 = int(input('Enter side A: '))
+num2 = int(input('Enter side B: '))
+
+
+# Process
+
+total = pow(num1, 2) + pow(num2, 2)
+
+
+# Output
+print('The length of side C is ', str(total))
